f25620581a1d9e0920d451134d61d5a8f627fb8e
mesjidnurulhikmahppmm-beep <mesjidnurulhikmah.ppmm@gmail.com>
2026-04-16 17:10:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,5 +14,5 @@
  * 5. Tempel URL tersebut pada API_URL di bawah ini
  */
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzQjEkrKLr2QCbVe0m6CFFCclBSe7X9sJ8Pux4lkK9nxwyDkYcKrPclpqmSU8PleHgH5g/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbz9qOc-fCElVt1NDDHFHxtJf8b9HXwyLiRkABJaZkiPnEtm97COr71YgDEZC7ATO-ei/exec"
 };
